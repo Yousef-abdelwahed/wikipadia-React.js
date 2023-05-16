765df2f1c04a6948f14f50a2edada2c2a720e28d
Yousef-abdelwahed <95501008+Yousef-abdelwahed@users.noreply.github.com>
2023-05-16 22:30:45 +0300

    add protect from xxs attach

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+const App = () => {
+  const [terms, setTerms] = useState("");
+  const [resalt, setResalt] = useState([]);
+  useEffect(() => {
+    const search = async () => {
+      const respond = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: terms,
+        },
+      });
+      let query = respond.data.query.search;
+      setResalt(query);
+    };
+    return () => {
+      if (terms) {
+        search();
+      }
+    };
+  }, [terms]);
+  const fetchResults = resalt.map((el) => {
+    return (
+      <tr key={el.pageid}>
+        <td>{el.id}</td>
+        <td>{el.title}</td>
+        <td>{el.snippet}</td>
+        <td>
+          <span dangerouslySetInnerHTML={ />
+        </td>
+      </tr>
+    );
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col ">
+            <label className="my-2 fw-bold" htmlFor="searcj">
+              Search
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Please Enter title "
+              required
+              onChange={(e) => setTerms(e.target.value)}
+              value={terms}
+            ></input>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="col">
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Desc</th>
+              </tr>
+            </thead>
+            <tbody>{fetchResults}</tbody>
+          </table>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
